a4c2ecaf139e159fd41016d1fca49972ad055828
rynkow <rynkowski.sz@gmail.com>
2021-11-16 15:11:23 +0100

    zadania

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,21 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Task servant_task = new Task() {
+        Task servantTask = new Task() {
             @Override
             public void run() {
                 double a = Math.sin(12.65);
             }
         };
 
-        Task producer_task = new Task() {
+        Task producerTask = new Task() {
             @Override
             public void run() {
                 double a = Math.sin(83.55);
             }
         };
 
-        Task consumer_task = new Task() {
+        Task consumerTask = new Task() {
             @Override
             public void run() {
                 double a = Math.sin(42.64);
@@ -35,15 +35,15 @@ public class Main {
         int noOfConsumers = 10;
         int bufferCapacity = 10;
 
-        Proxy proxy = new Proxy(bufferCapacity, servant_task);
+        Proxy proxy = new Proxy(bufferCapacity, servantTask);
 
         List<Producer> producers = new ArrayList<>();
         List<Consumer> consumers = new ArrayList<>();
 
         for (int i = 0; i < noOfProducers; i++)
-            producers.add(new Producer(i, proxy, bufferCapacity / 2, producer_task));
+            producers.add(new Producer(i, proxy, bufferCapacity / 2, producerTask));
         for (int i = 0; i < noOfConsumers; i++)
-            consumers.add(new Consumer(i, proxy, bufferCapacity / 2, consumer_task));
+            consumers.add(new Consumer(i, proxy, bufferCapacity / 2, consumerTask));
 
         for (Producer producer : producers)
             producer.start();
